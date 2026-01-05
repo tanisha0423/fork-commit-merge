8a5b2f31de72964470cf9f90f9d0e7af32d1b09e
tanisha <161014421+tanisha0423@users.noreply.github.com>
2026-01-06 01:56:02 +0530

    Add function to sum two numbers
    Implemented a function to calculate and print the sum of two numbers.

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,3 +1,9 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
+def sum_of_two(n,m):
+  sum = n + m
+  print(sum)
+n = int(input())
+m = int(input())
+sume_of_two(n,m)
